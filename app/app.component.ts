c9eea6ca7375723abe5df424c543c36d1a3c81f6
ilian1902 <ilian.bratanov82@gmail.com>
2017-01-24 11:35:30 +0200

    Test geolocation.

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,10 +6,21 @@ import geolocation = require("nativescript-geolocation");
     templateUrl: "app.component.html",
 })
 export class AppComponent {
-    
-        public enableLocationTap() { 
+    onInit(){
+    }
+    public enableLocationTap() { 
         if (!geolocation.isEnabled()) {
             geolocation.enableLocationRequest();
         }
     }
+    public buttonGetLocationTap() {
+    var location = geolocation.getCurrentLocation({desiredAccuracy: 3, updateDistance: 10, maximumAge: 20000, timeout: 20000}).
+    then(function(loc) {
+        if (loc) {
+            console.log("Current location is: " + loc);
+        }
+    }, function(e){
+        console.log("Error: " + e.message);
+    });
+}
 }
